77c461559a1b0e35bac2f62666de86662427ff11
ljy <4175459@qq.com>
2024-06-13 20:15:43 +0800

    feat: 修改菜单

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-require('dotenv')
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 const createWindow = () => {
   const win = new BrowserWindow({ width: 800, height: 600 });
   win.loadFile("index.html");
 };
 
+const createMenu = () => {
+  const template = [
+    { label: "菜单一", submenu: [{ label: "功能一" }, { label: "功能二" }] },
+    { label: "菜单二", submenu: [{ label: "功能一" }, { label: "功能二" }] },
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu)
+};
+
 app.whenReady().then(() => {
   createWindow();
+  createMenu();
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
+      createMenu();
     }
   });
 });
